511f66b613a21c657e42a2b45de74c60e11de249
Lorraine Morrissey <lorraine.morrissey@atu.ie>
2025-04-29 13:11:57 +0100

    Lab 3.1

---
+++ Week3/Extra.py
@@ -0,0 +1,13 @@
+# Extra.py
+# Lab 3.1 Extra questions
+# Author: Lorraine Morrissey
+
+# Original expression
+'''
+message = 'I have eaten' + 99 + ' burritos'
+print (message)
+'''
+# Correction
+
+number_eaten = 99
+print ("I have eaten {} burritos".format(number_eaten))
